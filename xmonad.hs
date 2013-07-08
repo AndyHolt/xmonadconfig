5ef183cf4550b1a3b595a619454d1727e8523490
Andy Holt <andrew.holt@hotmail.co.uk>
2013-07-08 16:38:26 +0100

    Add synapse support and key bindings.
    Add synapse to list of programs that start on startup. Synapse is a
    launcher with a pop-up gui to find files and applications etc.
    Added imports to xmonad.hs, workspace setup and keybindings.

--- xmonad.hs
+++ xmonad.hs
@@ -9,10 +9,22 @@
 -}
 
 import XMonad
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.Grid
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Circle
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.Fullscreen
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+import XMonad.Actions.Plane
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import Data.Ratio ((%))
 import System.IO
 
 {-
@@ -59,6 +71,61 @@ myWorkspaces =
 
 startupWorkspace = "1:Term"  -- this is where to start after launch
 
+{-
+  Layout configuration. In this section, we identify which xmonad layouts to
+  use.
+  List of default layouts are applied to every window.
+  Special layouts are applied to specific layouts.
+
+  Note: all layouts are wrapped in "avoidStruts" - this makes the layouts avoid
+  the status bar at the top of the screen. Behaviour can be toggled with
+  keybinding.
+-}
+
+-- Define the group of default layouts, in the order they appear.
+-- "smartBorders" modifier makes the borders only appear if there is more than
+-- one visible window.
+defaultLayouts = smartBorders(avoidStruts(
+  -- ResizableTall layout has a large master window on left and remaining
+  -- windows tile on the RHS. By default, each area takes up half of screen, but
+  -- can be resized using h and l keys.
+  ResizableTall 1 (3/100) (1/2) []
+
+  -- Mirrored variation of ResizableTall - large master window on top, remaining
+  -- windows tile at bottom of the screen. Resized in same way.
+  ||| Mirror (ResizableTall 1 (3/100) (1/2) [] )
+
+  -- Full layout makes every window full screen.
+  ||| noBorders Full
+
+  -- Grid layout tried to equally distribute windows in the availiable space,
+  -- increasing the number of columns and rows as necessary.
+  -- Master window is at top left.
+  ||| Grid
+
+  -- ThreeColMid layout puts the large master window in the center of the
+  -- screen. As configured, by default it takes up 3/4 of the availiable space.
+  ||| ThreeColMid 1 (3/100) (3/4)
+
+  -- Circle layout places the master window in the centre of the screen.
+  -- Remaining windows appear in a circle around it
+  ||| Circle))
+
+myKeyBindings =
+  [
+    ((myModMask, xK_f), sendMessage ToggleStruts)
+  , ((myModMask, xK_a), sendMessage MirrorShrink)
+  , ((myModMask, xK_z), sendMessage MirrorShrink)
+  , ((myModMask, xK_s), spawn "synapse")
+--  , ((myModMask, xK_u), focusUrgent)
+  , ((0, 0x1008ff12), spawn "amixer -D pulse set Master toggle")
+  , ((0, 0x1008ff11), spawn "amixer -q set Master 5%-")
+  , ((0, 0x1008ff13), spawn "amixer -q set Master 5%+")
+  , ((mod4Mask, xK_w), spawn "gnome-screensaver-command -l")
+  , ((mod4Mask, xK_c), spawn "emacsclient -c -e '(org-capture)'") 
+    ]
+
+
 myManageHook = composeAll
   [ className =? "Gimp"      --> doFloat
   , className =? "Vncviewer" --> doFloat
@@ -87,8 +154,5 @@ main = do
             }
     , startupHook = do
         spawn "~/.xmonad/startup-hook"
-    } `additionalKeys`
-    [ -- lock screen
-        ((mod4Mask, xK_L), spawn "gnome-screensaver-command -l")
-      , ((mod4Mask, xK_c), spawn "emacsclient -c -e '(org-capture)'") 
-    ]
+    }
+      `additionalKeys` myKeyBindings
